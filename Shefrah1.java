247fc3c7b5c6e390d7907c4eb965393d35cd7202
Raghad Alhulwah <164327849+RaghadAlhulwah@users.noreply.github.com>
2025-02-16 20:29:46 +0300

    Add files via upload

---
+++ Shefrah1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shefrah1;
+
+/**
+ *
+ * @author raghadalhulwah
+ */
+public class Shefrah1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
